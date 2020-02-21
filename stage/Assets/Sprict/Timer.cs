2fb07b604aa0be1543cf265c00699909ae606eff
natsu-810 <61263562+natsu-810@users.noreply.github.com>
2020-02-21 11:29:31 +0900

    ステージ
    まだ仮です

---
+++ stage/Assets/Sprict/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+        float countTime = 100;
+
+        // Update is called once per frame
+        void Update()
+        {
+            countTime -= Time.deltaTime;
+            GetComponent<Text>().text = countTime.ToString("F2");
+        　　
+        　　if(countTime == 0)
+            {
+            SceneManager.LoadScene("ここにゲームオーバー(タイムアップ？)のシーン名を入力");
+            }
+        }
+
+}
