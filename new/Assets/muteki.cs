770c75ef4c4ebb47e0296c42ffcde90704c48697
momomo131 <jyuri131@icloud.com>
2020-02-21 11:16:23 +0900

    作りかけプレイヤー
    つくりかけですまない。。。

---
+++ new/Assets/muteki.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class muteki : MonoBehaviour
+{
+    private bool isDamaged = false; //敵に当たったか
+
+    // Use this for initialization
+    void Start()
+    {
+        isDamaged = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        //ダメージを受けた時の処理
+        if (isDamaged)
+        {
+            float level = Mathf.Abs(Mathf.Sin(Time.time * 10));
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, level);
+        }
+    }
+}
