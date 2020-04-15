3aa11921907aa62a84856e12681a47eff159fb4e
natsu-810 <61263562+natsu-810@users.noreply.github.com>
2020-04-15 15:16:18 +0900

    ステージ(途中)
    進捗

---
+++ stage_2/Assets/Script/Item.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    //アイテムにアタッチする前提です。(タグ付けもしておくとなお良し)
+
+    // トリガーとの接触時に呼ばれるコールバック
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        // 接触対象がプレイヤーかどうかを判断する
+        if (hit.CompareTag("ここにプレイヤーのタグを入力する"))
+        {
+            // アイテムをDestroy、取ったように見せかける
+            Destroy(gameObject);
+
+            //プレイヤーがアイテム(バケツ？)に触れる＝水ゲージの回復
+            //アイテムを取得することにより水回復の処理
+
+
+        }
+    }
+}
