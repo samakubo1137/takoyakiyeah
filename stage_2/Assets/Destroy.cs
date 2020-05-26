d637ede06be006dcadff807ce06f3e6bc3411557
Thun27 <60684588+Thun27@users.noreply.github.com>
2020-05-26 15:52:34 +0900

    エフェクト＆敵配置
    ステージ２

---
+++ stage_2/Assets/Destroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        // 弾とエネミーオブジェクトを消滅させる
+        Destroy(collider.gameObject);   // 弾オブジェクト消去
+        Destroy(gameObject);            // 自オブジェクト消去
+    }
+}
