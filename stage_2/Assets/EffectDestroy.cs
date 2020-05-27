310ebcf30f743dd275308dbcf0e5b3fffc9adda7
Thun27 <60684588+Thun27@users.noreply.github.com>
2020-05-27 12:41:23 +0900

    衝突の文字は出た、

--- stage_2/Assets/EffectDestroy.cs
+++ stage_2/Assets/EffectDestroy.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class EffectDestroy : MonoBehaviour
 {
+    //private int count;
+
     // Start is called before the first frame update
-    void Start()
+    //private void Start()
+    //{
+    //    count = 0;
+    //}
+
+    void OnParticleCollision(GameObject ogj)
     {
-
+        Debug.Log("衝突");
     }
-
     // Update is called once per frame
     void Update()
     {
 
     }
+
+    //private void OnParticleCollision(GameObject other)
+    //{
+    //    count++;
+    //}
+
+    //public int Count
+    //{
+    //    get { return count; }
+    //    private set { count = value; }
+    //}
+    
 }
 
