0d78dd6723d21d41a5b1817596c11342ab304465
momomo131 <jyuri131@icloud.com>
2020-05-29 12:22:08 +0900

    a

--- stage_2/Assets/water.cs
+++ stage_2/Assets/water.cs
@@ -5,22 +5,30 @@ using UnityEngine.UI;
 
 public class water : MonoBehaviour
 {
-   
+    float _water = 100;
     Slider _slider;
 
     // Start is called before the first frame update
     void Start()
     {
         _slider = GameObject.Find("Slider").GetComponent<Slider>();
-    }
 
-    float _water = 100;
+    }
+    
+
+    
 
     // Update is called once per frame
     void Update()
     {
-       if()
+        if (Input.GetKey(KeyCode.A))
+        {
+            _water -= -5;
+        }
 
     }
+
+    
+
 }
 
