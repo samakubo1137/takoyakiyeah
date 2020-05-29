494da9e1acd38ac91d041ff02ccd8b47e302dcbc
senami07 <akameganedayo8@gmail.com>
2020-05-29 12:49:32 +0900

    メニュースプ

---
+++ stage_2/Assets/Script/scene3.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scene3 : MonoBehaviour
+{
+    //　アイテムメニューを開くボタン
+    [SerializeField]
+    private GameObject itemButton;
+    //　ゲーム再開ボタン
+    [SerializeField]
+    private GameObject reStartButton;
+    //　アイテムメニューパネル
+    [SerializeField]
+    private GameObject itemPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    public void StopGame()
+    {
+        Time.timeScale = 0f;
+        itemButton.SetActive(false);
+        reStartButton.SetActive(true);
+        itemPanel.SetActive(true);
+    }
+
+    public void ReStartGame()
+    {
+        itemPanel.SetActive(false);
+        reStartButton.SetActive(false);
+        itemButton.SetActive(true);
+        Time.timeScale = 1f;
+
+    }
+        // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
