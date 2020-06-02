5c4c3a8a8693b8e7c28d6b41034815032a1107ca
momomo131 <jyuri131@icloud.com>
2020-06-02 10:36:47 +0900

    ｈｐできたかもしれん

--- stage_2/Assets/HP.cs
+++ stage_2/Assets/HP.cs
@@ -24,7 +24,7 @@ public class hp : MonoBehaviour
         _slider.value = HP;
     }
 
-    void OnColliderEnter2D(Collider collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
