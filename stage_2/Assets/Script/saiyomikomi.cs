3187757024dfdd11aee92a8a5c77bf23cd398527
senami07 <akameganedayo8@gmail.com>
2020-06-03 11:19:30 +0900

    リトライ

---
+++ stage_2/Assets/Script/saiyomikomi.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;//クラス名の上に追加してください
+
+
+public class saiyomikomi : MonoBehaviour
+{
+    string waaa;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waaa = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RetryButton()
+    {
+        SceneManager.LoadScene(waaa);
+    }
+
+}
