5f6b34cc9ed00923fb22bcdfb7a7d51e3425636b
senami07 <akameganedayo8@gmail.com>
2020-06-03 11:28:50 +0900

    タイトルへ

---
+++ stage_2/Assets/Script/taitoruhe.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class taitoruhe : MonoBehaviour
+{
+    public void StartGame()
+    {
+		SceneManager.LoadScene ("boss");
+	}
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
