b3460b16253a90a13ca07d2fcbb27d6b15a8bb73
senami07 <akameganedayo8@gmail.com>
2020-06-10 15:00:45 +0900

    わあああああい

---
+++ stage_2/Assets/Script/sceneikou.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class sceneikou : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // マウスを左クリックした瞬間
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("BOSS");
+        }
+    }
+}
