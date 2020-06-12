43d428663659a64f24ab9091661a835c6952becb
momomo131 <jyuri131@icloud.com>
2020-06-12 10:27:27 +0900

    あ

---
+++ stage_2/Assets/BossText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossText : MonoBehaviour
+{
+    private float step_time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        step_time = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        step_time += Time.deltaTime;
+        if (step_time >= 3.0f)
+        {
+            SceneManager.LoadScene("ClaerScene");
+        }
+    }
+}
+
--- stage_2/Assets/EnemyHelth.cs
+++ stage_2/Assets/EnemyHelth.cs
@@ -1,38 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class EnemyHelth : MonoBehaviour
 {
 
-    private float hp = 5;
+    private float hp = 10;
+
+    public Text GoalText;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //水に当たった時の処理
-        if(collision.gameObject.tag=="Water")
+        if (collision.gameObject.tag == "Water")
         {
             Debug.Log("hit");
             hp -= 1;
         }
 
         //体力が０以下になった時の処理
-        if(hp<=0)
+        if (hp <= 0)
         {
-            Destroy(gameObject);　//ゲームオブジェクト破壊
+            Destroy(gameObject); //ゲームオブジェクト破壊
+            GoalText.enabled = true;
+
         }
 
-        
+
+
+
+
     }
+    
     
 }
